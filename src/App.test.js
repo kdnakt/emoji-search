02eb62451c68268dd99cda6bb2b4d01e48856745
kdnakt <a.kid.1985@gmail.com>
2018-01-07 01:02:05 +0900

    add snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import renderer from 'react-test-renderer';
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const app = renderer.create(<App />);
+  expect(app.toJSON()).toMatchSnapshot();
 });
